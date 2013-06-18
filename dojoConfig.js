0505eae6bc6a5249665a380d35525a1dcb33aa2b
Blaine Bublitz <blaine@iceddev.com>
2013-06-17 20:59:02 -0700

    add new deps

--- dojoConfig.js
+++ dojoConfig.js
@@ -1,25 +1,12 @@
 var dojoConfig = {
   baseUrl: './',
   packages: [
-    {
-      name: 'dojo',
-      location: 'deps/dojo'
-    },
-    {
-      name: 'dcl',
-      location: 'deps/dcl',
-      main: 'dcl'
-    },
-    {
-      name: 'frozen',
-      location: 'deps/frozen/src',
-      main: 'GameCore'
-    },
-    {
-      name: 'game',
-      location: 'src',
-      main: 'game'
-    }
+    { name: 'dojo', location: 'deps/dojo' },
+    { name: 'dcl', location: 'deps/dcl', main: 'dcl' },
+    { name: 'lodash', location: 'deps/lodash/dist', main: 'lodash' },
+    { name: 'hammer', location: 'deps/hammer', main: 'hammer' },
+    { name: 'frozen', location: 'deps/frozen/src', main: 'GameCore' },
+    { name: 'game', location: 'src', main: 'game' }
   ],
   deps: ['game'],
   async: true
